45dfbec33d295f0f274a108fde165cba2ae3068b
Yu Zhang <43693831+JohnWillian@users.noreply.github.com>
2021-06-07 21:52:56 +0800

    first commit

---
+++ src/components/UserForm.vue
@@ -0,0 +1,212 @@
+<template>
+    <a-table :columns="columns" :data-source="data">
+        <template #customTitle>
+            <span>
+                <smile-outlined />
+                Name
+            </span>
+        </template>
+        <template #name="{ text: fuck }">
+            <a>{{ fuck }}</a>
+        </template>
+        <template #tags="{ text: tags }">
+            <span>
+                <a-tag v-for="tag in tags" :key="tag" :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'">
+                    {{ tag.toUpperCase() }}
+                </a-tag>
+            </span>
+        </template>
+        <template #action="{ record }">
+            <span>
+                <a data-record="123" @click="onDelete(record,$event)">删除</a>
+                <a-divider type="vertical" />
+                <a @click="handleAdd">编辑</a>
+            </span>
+        </template>
+    </a-table>
+    <a-button class="editable-add-btn" @click="handleAdd" style="margin-bottom: 8px">Add</a-button>
+</template>
+<script>
+import { SmileOutlined } from '@ant-design/icons-vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    // setup() {
+    //     const columns = [
+    //         // {
+    //         //         dataIndex: 'name',
+    //         //         key: 'name',
+    //         //         slots: {
+    //         //             title: 'customTitle',
+    //         //             customRender: 'name',
+    //         //         },
+    //         //     },
+    //         {
+    //             // title: 'Name',
+    //             dataIndex: 'name',
+    //             key: 'name',
+    //             slots: {
+    //                 title: 'customTitle',
+    //                 customRender: 'name'
+    //             }
+    //         },
+    //         {
+    //             title: 'Age',
+    //             dataIndex: 'age',
+    //             key: 'age',
+    //         },
+    //         {
+    //             title: 'Address',
+    //             dataIndex: 'address',
+    //             key: 'address',
+    //         },
+    //         {
+    //             title: 'Tags',
+    //             key: 'tags',
+    //             dataIndex: 'tags',
+    //             slots: {
+    //                 customRender: 'tags',
+    //             },
+    //         },
+    //         {
+    //             title: 'Action',
+    //             key: 'action',
+    //             slots: {
+    //                 customRender: 'action',
+    //             },
+    //         },
+    //     ];
+    //     const data = ref([{
+    //             id: 0,
+    //             key: '1',
+    //             name: 'John Brown',
+    //             age: 32,
+    //             address: 'New York No. 1 Lake Park',
+    //             tags: ['nice', 'developer'],
+    //         },
+    //         {
+    //             id: 1,
+    //             key: '2',
+    //             name: 'Jim Green',
+    //             age: 42,
+    //             address: 'London No. 1 Lake Park',
+    //             tags: ['loser'],
+    //         },
+    //         {
+    //             id: 2,
+    //             key: '3',
+    //             name: 'Joe Black',
+    //             age: 32,
+    //             address: 'Sidney No. 1 Lake Park',
+    //             tags: ['cool', 'teacher'],
+    //         },
+    //     ]);
+
+    //     const count = computed(() => data.value.length + 1);
+
+    //     const onDelete = key => {
+    //         console.log("length: ", data.value.length)
+    //         console.log("Key: ", key)
+    //         data.value = data.value.filter(item => item.key !== key);
+    //     };
+
+    //     const handleAdd = () => {
+    //         const newData = {
+    //             id: `${count.value - 1}`,
+    //             key: `${count.value}`,
+    //             name: 'Coco',
+    //             age: 22,
+    //             address: 'Sidney No. 1 Lake Park',
+    //             tags: ['sex', 'Fucker'],
+    //         };
+    //         data.value.push(newData);
+    //     };
+
+    //     return {
+    //         data,
+    //         columns,
+    //         count,
+    //         onDelete,
+    //         handleAdd,
+    //     };
+    // },
+
+    data(){
+        return{
+            data: [{
+                id: 0,
+                key: '1',
+                name: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park',
+                tags: ['nice', 'developer'],
+            },
+            {
+                id: 1,
+                key: '2',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+                tags: ['loser'],
+            },
+            {
+                id: 2,
+                key: '3',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+                tags: ['cool', 'teacher'],
+            }],
+
+            columns: [{
+                // title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+                slots: {
+                    title: 'customTitle',
+                    customRender: 'name'
+                }
+            },
+            {
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age',
+            },
+            {
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
+            },
+            {
+                title: 'Tags',
+                key: 'tags',
+                dataIndex: 'tags',
+                slots: {
+                    customRender: 'tags',
+                },
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                slots: {
+                    customRender: 'action',
+                },
+            }]
+        }
+    },
+
+    methods:{
+        onDelete(record,e){
+            console.log(record);
+            console.log(e.srcElement.dataset.record);
+        },
+        handleAdd(){
+           
+        }
+    },
+
+    components: {
+        SmileOutlined,
+    }
+});
+</script>
---
+++ src/components/test.vue
@@ -0,0 +1,56 @@
+<template>
+    <a-layout class="layout">
+        <a-layout-content style="padding: 0 50px">
+            <a-breadcrumb style="margin: 16px 0">
+                <a-breadcrumb-item>用户信息</a-breadcrumb-item>
+            </a-breadcrumb>
+            <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+                <UserForm></UserForm>
+            </div>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+            ©2021 Created by Yu Zhang
+        </a-layout-footer>
+    </a-layout>
+</template>
+<script>
+    import UserForm from './UserForm.vue'
+    import {
+        defineComponent,
+        ref
+    } from 'vue';
+    export default defineComponent({
+        setup() {
+            return {
+                selectedKeys: ref(['2']),
+            };
+        },
+        components: {
+            UserForm
+        }
+    });
+</script>
+<style>
+    .site-layout-content {
+        min-height: 280px;
+        padding: 24px;
+        background: #fff;
+    }
+
+    #components-layout-demo-top .logo {
+        float: left;
+        width: 120px;
+        height: 31px;
+        margin: 16px 24px 16px 0;
+        background: rgba(255, 255, 255, 0.3);
+    }
+
+    .ant-row-rtl #components-layout-demo-top .logo {
+        float: right;
+        margin: 16px 0 16px 24px;
+    }
+
+    [data-theme='dark'] .site-layout-content {
+        background: #141414;
+    }
+</style>
